e078641a2fe1c290373c3ba6bfa182e08ff25f91
dezz2201 <zachary.deziel@usherbrooke.ca>
2020-11-28 15:58:19 -0500

    added custom greeting with additionnal name

--- src/main.go
+++ src/main.go
@@ -1,21 +1,42 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello World!")
-    fmt.Println("Endpoint Hit: Hello World")
+var name string
+
+func getGreeting() string {
+	if name == "" {
+		return "Hello World!"
+	}
+
+	return "Hello " + name + "!"
 }
 
-func handleRequests() {
-    http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":9090", nil))
+func homePage(w http.ResponseWriter, r *http.Request) {
+	names, ok := r.URL.Query()["name"]
+
+	// helloworld if name is not present
+	if !ok || len(names[0]) < 1 {
+		log.Println("Url Param 'name' is missing. Showing hello world!")
+		name = ""
+	} else {
+		name = names[0]
+		log.Println("Url Param 'name' is: " + string(name))
+	}
+
+	fmt.Fprintf(w, getGreeting())
+	fmt.Println("Endpoint Hit: Homepage")
+}
+
+func handlerHomePage() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":9090", nil))
 }
 
 func main() {
-    handleRequests()
+	handlerHomePage()
 }
